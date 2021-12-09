93188a3449c76b1c224c1f3175d23623114f5bfd
kyL_lee <leekwanyao710@gmail.com>
2021-12-09 19:35:20 +0800

    Fix when change the size, SPEED will Fast UP

--- MomBirthday/JavaScript/HBM_Game_1.js
+++ MomBirthday/JavaScript/HBM_Game_1.js
@@ -11,11 +11,11 @@ window.onload = function(){
         }
     });
 }
-
+/*
 window.onresize = function(){
     Canvas_Reset();
 }
-
+*/
 function Canvas_Reset(){
     var Canvas_width = document.body.clientWidth * 0.9;
     var Canvas_height = document.body.clientHeight * 0.7;
